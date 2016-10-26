243e39cf63e9faf834fe38455714b71b59c9c207
zhuanbang <79228807@qq.com>
2016-10-26 13:12:34 +0800

    MODI:button&&ADD:text-link

---
+++ wui/copy-to-shear-board.js
@@ -0,0 +1,9 @@
+/*点击按钮复制到剪贴板*/
+var $page=$("#page-demo");
+$page.on("focus",".copy-to-shear-board",function(){
+    $(this).css({ "background-color": "#5eb95e" }).select();
+    document.execCommand("Copy")
+});
+$page.on("blur",".copy-to-shear-board",function(){
+    $(this).css({ "background-color": "#fff" });
+});
