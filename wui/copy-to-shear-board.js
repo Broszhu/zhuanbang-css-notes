91255486ad0f21a3604d601e39bad4308f9c7b93
zhuanbang <79228807@qq.com>
2016-10-27 18:42:48 +0800

    form

--- wui/copy-to-shear-board.js
+++ wui/copy-to-shear-board.js
@@ -1,9 +1,11 @@
 /*点击按钮复制到剪贴板*/
 var $page=$("#page-demo");
 $page.on("focus",".copy-to-shear-board",function(){
-    $(this).css({ "background-color": "#5eb95e" }).select();
+    //$(this).css({ "background-color": "#5eb95e" }).select();
+    $(this).select();
     document.execCommand("Copy")
 });
+/*
 $page.on("blur",".copy-to-shear-board",function(){
     $(this).css({ "background-color": "#fff" });
-});
+});*/
